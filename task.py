f4880eba4818733896cdf9106307bd6bc6682236
赵已绩 <zhaoyiji@hikvision.com.cn>
2016-06-14 20:04:30 +0800

    [mod] [add content, Period, PeriodMod.]

--- task.py
+++ task.py
@@ -1,5 +1,10 @@
 from datetime import datetime
 
+PERIOD_DAY = 1
+PERIOD_WEEK = 2
+PERIOD_MON = 4
+PERIOD_YEAR = 8
+
 
 class Task(object):
     def __init__(self):
@@ -10,3 +15,39 @@ class Task(object):
         self.period = False
         self.notification = False
         self.content = ""
+
+
+class PeriodMode(object):
+    def __init__(self, mode):
+        self.mode = mode
+
+
+class PeriodModeDay(PeriodMode):
+    def __init__(self):
+        PeriodMode.__init__(self, PERIOD_DAY)
+
+
+class PeriodModeWeek(PeriodMode):
+    def __init__(self):
+        PeriodMode.__init__(self, PERIOD_WEEK)
+
+
+class PeriodModeMon(PeriodMode):
+    def __init__(self):
+        PeriodMode.__init__(self, PERIOD_MON)
+
+
+class PeriodModeYear(PeriodMode):
+    def __init__(self):
+        PeriodMode.__init__(self, PERIOD_YEAR)
+
+
+class Period(object):
+    def __init__(self):
+        self.mode = PERIOD_DAY
+        self.cycle = False
+        self.datetime_start = datetime.now()
+        self.datetime_end = datetime.now()
+        self.deadline = False
+        self.date_start = datetime.now().date()
+        self.date_end = datetime.now().date()
